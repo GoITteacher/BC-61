09a9a9de20fb6135f0efc3e4c0fec7e9d91dd370
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-09-13 12:44:19 +0300

    update

--- src/lesson/2-ptototype.js
+++ src/lesson/2-ptototype.js
@@ -1,3 +1,22 @@
 //Прототипне наслідування
 
-// Object;
+// const obj1 = {
+//   x: 1,
+//   y: 2,
+//   q: 3,
+// };
+
+// const obj2 = Object.create(obj1)
+
+// const obj2 = {
+//   t: 0,
+//   u: 6,
+//   l: 8,
+//   __proto__: obj1,
+// };
+
+// for (let key in obj2) {
+//   console.log(key);
+// }
+
+// ===============================
--- src/lesson/3-class.js
+++ src/lesson/3-class.js
@@ -14,6 +14,241 @@
 
 */
 
+/* class User {
+  #password;
+  #login;
+
+  constructor({ name = '', age = 1, phone = '', pass, login }) {
+    this.name = name;
+    this.age = age > 0 ? age : 0;
+    this.phoneNumber = phone;
+    this.#password = pass;
+    this.#login = login;
+  }
+  showName() {
+    console.log(this.name);
+  }
+  showAge() {
+    console.log(this.age);
+  }
+  showInfo() {
+    console.log('------------------');
+    console.log(this.#getInfo());
+    console.log('------------------');
+  }
+  showPassword() {
+    console.log('Password:', this.#password);
+  }
+  setPassword(newPassword) {
+    if (newPassword.length < 6) return;
+    this.#password = newPassword;
+  }
+  #getInfo() {
+    return `${this.name}\n${this.age}\n${this.phoneNumber}\n`;
+  }
+} */
+
+/* const user1 = new User({
+  age: 20,
+  name: 'max',
+  pass: '4564564',
+  login: 'testLogin',
+});
+
+const user2 = new User({
+  name: 'Nika',
+  age: -10,
+  phone: '234234',
+  pass: '123123',
+  login: 'barbariska',
+});
+
+user1.showInfo();
+user1.setPassword('new n');
+user1.setPassword('new n1');
+user1.showPassword();
+user2.showPassword(); */
+
+// ====================================
+/* 
+class User {
+  #age;
+  #name;
+  #phone = 'Default';
+
+  constructor(name, age) {
+    this.#name = name;
+    this.#age = age;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(name) {
+    if (name.length > 1) {
+      this.#name = name;
+    }
+  }
+
+  get age() {
+    return `Age: ${this.#age}`;
+  }
+
+  set age(age) {
+    if (age > 0) {
+      this.#age = age;
+    } else {
+      console.log('Ти шо???');
+    }
+  }
+
+  get phone() {
+    return this.#phone;
+  }
+
+  set phone(newPhone) {
+    this.#phone = newPhone;
+  }
+
+  get test() {
+    return 'HELLO';
+  }
+}
+
+const user = new User('Vova', 23);
+
+user.age = 10;
+console.log(user.age);
+
+user.age = -50;
+console.log(user.age);
+
+console.log(user.test); */
+
+// ======================================
+
+// class BankAccount {
+//   static #bankName = 'MonoBank';
+//   static #count = 1;
+
+//   constructor(fullName) {
+//     this.accountNumber = BankAccount.#count++;
+//     this.balance = 0;
+//     this.owner = fullName;
+//   }
+
+//   static getBankInfo() {
+//     console.log(`Bank: ${this.#bankName}\nAccountAmount: ${this.#count - 1}`);
+//   }
+
+//   getInfo() {
+//     console.log(`Bank: ${this.#bankName}\nAccountAmount: ${this.#count - 1}`);
+//   }
+
+//   static get() {
+//     return BankAccount.#bankName;
+//   }
+// }
+
+// const account1 = new BankAccount('Full Name 1');
+// const account2 = new BankAccount('Full Name 2');
+// const account3 = new BankAccount('Full Name 3');
+// const account4 = new BankAccount('Full Name 4');
+// const account5 = new BankAccount('Full Name 5');
+// const account6 = new BankAccount('Full Name 6');
+
+// console.log(account1);
+// console.log(account2);
+// console.log(account3);
+// console.log(account4);
+// console.log(account5);
+// console.log(account6);
+
+// console.log(BankAccount.bankName);
+// BankAccount.getBankInfo();
+
+// ================================
+
+class Animal {
+  constructor(eyes, type) {
+    this.eyes = eyes;
+    this.type = type;
+  }
+
+  sleep() {
+    console.log(`${this.type} - Zzzz`);
+  }
+}
+
+class Wolf extends Animal {
+  constructor(pawns) {
+    super(2, 'Wolf');
+    this.pawns = pawns;
+  }
+
+  eat() {
+    console.log('Йду їсти мясо');
+  }
+
+  sayHello() {
+    console.log('Woof Woof');
+  }
+}
+
+class Spider extends Animal {
+  constructor(pawns) {
+    super(8, 'Spider');
+    this.pawns = pawns;
+  }
+
+  eat() {
+    console.log('Йду їсти муху');
+  }
+
+  sayHello() {
+    console.log('____');
+  }
+}
+
+class Cat extends Animal {
+  constructor(pawns) {
+    super(2, 'Сat');
+    this.pawns = pawns;
+  }
+
+  eat() {
+    console.log('Йду їсти корм');
+  }
+
+  sayHello() {
+    console.log('Myaw Myaw');
+  }
+
+  showInfo() {
+    console.log(this.eyes);
+    console.log(this.type);
+    console.log(this.pawns);
+  }
+}
+
+const wolf = new Wolf(4);
+const spider = new Spider(6);
+const cat = new Cat(4);
+
+wolf.sayHello();
+spider.sayHello();
+cat.sayHello();
+
+cat.sleep();
+wolf.sleep();
+spider.sleep();
+
+cat.showInfo();
+
+console.log(wolf);
+
+// ================================
 /* 
 Синтаксис літерала об'єкта дозволяє створити один об'єкт. Проте, часто потрібно створити багато однотипних об'єктів з однаковим набором властивостей, але різними значеннями і методами для взаємодії з ними. Все це потрібно робити динамічно, під час виконання програми. З цією метою використовують класи - спеціальний синтаксис оголошення функції для створення об'єктів.
 */

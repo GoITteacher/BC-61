cd501f30f473dba6b1eb0de4abda21c4b87b0570
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-09-22 12:17:23 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-import * as names from './scripts/module1';
+// import { x } from './scripts/module1';
+// import UserClass from './scripts/module2';
+// import { x1, x2 } from './scripts/module3';
+// import obj from './scripts/module3';
+// import * as props from './scripts/module3';
+// console.log(x1, x2);
+// console.log(obj);
+// console.log(props);
 
-console.log(names);
+// import simpleLightbox from 'simplelightbox';
+// new simpleLightbox('.some-element a', {
+//   /* options */
+// });
+
+// import { throttle, debounce } from 'lodash';
+// import throttle from 'lodash.throttle';
+// throttle();
+// debounce();
+
+// import { myThrottle } from './scripts/module1';
+// myThrottle();
+
+// import { MySimpleLightBox } from './scripts/module2';
+// const instance = new MySimpleLightBox();
---
+++ src/scripts/module3.js
@@ -0,0 +1,8 @@
+export const x1 = 10;
+export const x2 = 20;
+
+export default {
+  name: 'Module3',
+  age: 10,
+  phoneNumber: 123124,
+};

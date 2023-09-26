b34f64412ed8510de8389c00b6c95d79b3d165d8
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-09-26 09:29:29 +0300

    update

--- src/js/01-timeout.js
+++ src/js/01-timeout.js
@@ -2,21 +2,6 @@
  * Метод window.setTimeout(callback, delay, args)
  */
 
-const listId = [];
-
-for (let i = 0; i < 10; i++) {
-  const id = setTimeout(() => {
-    console.log(i);
-  }, i * 1000);
-
-  listId.push(id);
-}
-
 /*
  * Очистка таймаута с clearTimeout(timeoutId)
  */
-
-console.log(listId);
-const idForDelete = listId[5];
-
-clearTimeout(idForDelete);
